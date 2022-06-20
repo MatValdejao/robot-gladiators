e84a4b0de0e6de2ed1ecba5add89ee5ac8769659
Mateus <mateusvaldejao@Mateuss-MacBook-Pro.local>
2022-06-19 21:39:57 -0400

    added highscore

--- assets/js/game.js
+++ assets/js/game.js
@@ -103,19 +103,29 @@ var startGame = function() {
 }
 
 var endGame = function() {
-    if (playerInfo.health > 0) {
-        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+    window.alert("The game has now ended. Let's see how you did!")
+
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
+    }
+
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.getItem("name", playerInfo.name);
+        
+        alert(playerInfo.name + " now has the highscore of " + playerInfo.money + "!");
     }
     else {
-        window.alert("You've lost your robot in battle.");
+        alert(playerInfo.name + " did not beat the highscore of " + highScore + ". Maybe next time!");
     }
-   var playAgainConfirm = window.confirm("Would you like to play again?");
-   if (playAgainConfirm) {
-    startGame();
-   }
-   else {
-    window.alert("Thank you for playing Robot Gladiators! Come back soon!");
-   }
+    var playAgainConfirm = window.confirm("Would you like to play again?");
+    if (playAgainConfirm) {
+        startGame();
+    }
+    else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
 }
 
 var shop = function() {
